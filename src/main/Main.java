f3afa90daa894567467a7cfff74a4fcdf1841fdd
Douglas Gallardo <douglasgallardoz11@gmail.com>
2020-09-14 14:08:16 -0500

    SE ha implementado el metodo main que permite verificar si el triangulo es equilatero, isosceleso escaleno

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,50 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner leer = new Scanner (System.in);
+		
+		int ladoA=0, ladoB=0, ladoC=0;	
+		
+	    System.out.print("Ingresa lado A: ");
+	    ladoA=leer.nextInt();
+	    System.out.print("Ingresa lado B: ");
+	    ladoB=leer.nextInt();
+	    System.out.print("Ingresa lado C: ");
+	    ladoC=leer.nextInt();
+	    
+	    if (ladoA == ladoB)
+	    {
+	        if (ladoB == ladoC)
+	        {
+	            System.out.println("**Triangulo equilatero**"); 
+	        }else
+	        {
+	            System.out.println("**Triangulo isosceles**");
+	        }
+	    }else
+	    {
+	    	if(ladoA == ladoC) {
+	    		System.out.println("**Triangulo isosceles**");
+	    	}else {
+	    		if(ladoB == ladoC) {
+	    			System.out.println("**Triangulo isosceles**");
+	    		}else {
+	    			System.out.println("**Triangulo escaleno**");
+	    		}
+	    		
+	    	}
+	    }
+	    	
+		
 		
 		
 
 	}
+	
 
 }
