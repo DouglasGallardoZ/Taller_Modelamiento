58e12a778d03328ae8516958080ecb2baa754e74
Douglas Gallardo <douglasgallardoz11@gmail.com>
2020-09-16 20:26:31 -0500

    Se agrego nueva clase

--- src/ventanas/Ventana.java
+++ src/ventanas/Ventana.java
@@ -1,4 +1,4 @@
-package ventanas;
+	package ventanas;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -11,25 +11,7 @@ public class Ventana extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Ventana frame = new Ventana();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -37,6 +19,8 @@ public class Ventana extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		
 	}
 
 }
